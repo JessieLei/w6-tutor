290ffc77ae8b7539e37540b9515adad27f33c1dd
JessieLei <alen31002000@gmail.com>
2022-08-25 00:27:13 +0800

    Update 2022-08-24T16:27:11.100Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -7,6 +7,42 @@ $(".ham-icon").click(function (e) {
   e.preventDefault();
   $(".dropdown").toggleClass("active");
 });
+var swiper = new Swiper(".course-swiper", {
+  autoplay: {
+    disableOnInteraction: false,
+    delay: 0
+  },
+  speed: 1500,
+  loop: true,
+  spaceBetween: 16,
+  slidesPerView: 3,
+  //多欄
+  breakpoints: {
+    768: {
+      slidesPerView: 3
+    },
+    992: {
+      slidesPerView: 6
+    },
+    1200: {
+      slidesPerView: 9
+    }
+  }
+});
+var swiper2 = new Swiper(".feedbackSwiper", {
+  loop: true,
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  }
+});
 "use strict";
 
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
